e53638cd2e0b6e3478ee8c681a8e8360975c3d9e
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-27 16:24:01 +0900

    feat(component) BarChart

---
+++ chart/src/components/barChart.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactECharts from "echarts-for-react";
+
+const BarChart = () => {
+  const data = [20, 50, 80, 45, 60];
+
+  const option = {
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: data,
+        type: "bar",
+      },
+    ],
+  };
+
+  return (
+    <ReactECharts option={option} style={{ width: "100%", height: "400px" }} />
+  );
+};
+
+export default BarChart;
